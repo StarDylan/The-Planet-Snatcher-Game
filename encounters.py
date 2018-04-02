62c0daa0c262a5394fed661da98c40f847a8b7e9
Frank Dininno <frank.dininno@warriorlife.net>
2018-04-02 10:15:14 -0700

    added encounters replaced unwanted

---
+++ encounters.py
@@ -0,0 +1,120 @@
+
+
+def creature_enc():
+	x = random.randint(1,4)
+	if x == 1:
+		enc1()
+	if x == 2:
+		enc2()
+	if x == 3:
+		enc3()
+	if x == 4:
+		enc4()
+
+
+["a) RUN!!! |works - 50%|","b) Hide |works - {}%|","c) Encounter"]
+
+
+
+
+
+        
+
+def Print_1():
+    return '{}{}'.format(Print_1, .format(int(self.strength * 2), errorCode))
+
+
+
+def enc1(errorCode = ""):
+
+	if enc1 == 1
+		creature_enc()
+	print("""
+
+	While treading through a thick jungle, you meet behind to face with a
+	camouflage rhino. It seems to be slightly disturbed, but not aware of you.
+	What will you do?
+
+ 					  V V V
+					   V V
+
+			a) RUN!!! |works - 50%|
+			b) Hide |works - {}%|
+			c) Encounter
+
+		{}
+
+	""".format(int(self.clever * 2), errorCode))
+
+	response = askPrompt()
+		if response = "c"
+			print("""
+			The Beast looks at you wearily, wondering what you plan on doing with
+			drooping ears. It looks as if it is a calf separated from it's crash.
+
+							  V V V
+							   V V
+            
+			a) Throw him a small piece of your food and leave slowly (+ Wisdom)
+			b) Pet
+			c) KILL	\*
+			""".format(int(self.strength * 2), errorCode))
+
+			response = askPrompt()
+			if response = 'c'
+				do_atactk()
+
+			if atk == 1
+				print("""
+				As you charge toward it, you feel guilty of poaching.
+				You realize that it has evolved to hide from poachers
+				that kill rhinos for their horns. Finally, still with
+				a thoughtful doubt in your mind, you stab the beast and
+				it falls to the ground with a sad final shriek. Still feeling
+				inhumane, you take the rhino's horn.
+				""")
+			roll()
+
+			if atk == 0
+				print("""
+
+				As you prepare to attack the rhino.
+				In notices you, and with one swift
+				move, it charges you and knocks you
+				into a tree. You pass out immediately.
+
+				     	  `::-----------:.
+				       .::-              .::.
+				    `::.                    .:-
+				   ::                         `/.
+				  :-                            -/
+				  +./                           -:-
+				 :- y                          -+ +
+				 o `s                          .o o
+				 o o.                           s o
+				 o/: `..--`                     o.o
+				 ++.odNNNNmddo` `---.  .-/y++/:``.+
+				 .+:MMMMMMMMMMo ....:`+mMMMMMMMm//.
+				.:-:MMMMMMMMMMN. `    NMMMMMMMMMo/.
+				o` `dMMMMMMMMmo`+dd/ `NMMMMMMMMm`+`
+				o   .+yhddhs:. /NMMy  -yNMMMMNy. .+
+				:-             mMMMM:   .:+o+:   `+
+				 /.```        oMMMMMd            /`
+				  ..-+-/`     +NNmMNy      `````::
+				  .::+ so      ..`:-      o-++++`
+				  /.   ss`               -N `..+
+				   -/` +Mms:---:-.-:-.-:sdh  /-
+				     +`.MMm+oy.o.s`s++y+MM: /.
+				      +`+MMMhyosoyoss+NMMo :-
+				      `+ /dy+---...--:oy-  o
+				       :-                `+`
+				        -:--:--------:--::
+				""")
+			    time.sleep(1000000)
+
+		if response = 'b'
+            Print_1
+
+    
+
+#creature encounter

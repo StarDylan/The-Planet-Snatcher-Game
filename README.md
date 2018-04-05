6e36c7fad13aff7d25bad14344c82747eb8e9372
StarDylan <dylan@starink.com>
2018-04-05 14:47:09 -0700

    Added Random Groups
    Fixed Errors on Encounters.py
    
    Added support for groups for random scenes.

--- README.md
+++ README.md
@@ -1,30 +1,45 @@
+## Installation
+
 
 ## Configuration
 
-Editing the Story
---------------
+### Editing the Story
 
 
 The json structure is structured like this:
 
-{
-"RandomStories": [{
-  "TEXT": "INSERT RANDOM STORY",
-  "ID": " < OPTION ID > ",
-  "OPTIONS": [{
-    "SUCCESS": "DoSomething",
-    "FAILED": "DoSomethingElse",
-    "PERCENT": 5
-  }]
-}],
-"Stories": {
-  "DoSomething": "This Happens when RANDOM Story has succeceded!"
-},
+    {
+    	"RandomStories": {
+    		"<RandomGroupID>": [{
+    			"TEXT": "INSERT RANDOM STORY",
+    			"ID": " < OPTION ID > ",
+    			"OPTIONS": [{
+    				"SUCCESS": "DoSomething",
+    				"FAILED": "DoSomethingElse",
+    				"PERCENT": 5
+    			}]
+    		}]
+    	},
+    	"Stories": {
+    		"< StoryUID >": {
+    			"DATA": "INSERT RANDOM STORY",
+    			"ID": " < OPTION ID > ",
+    			"OPTIONS": {
+    				"SUCCESS": [{
+    					"SHOW": "< Insert StoryUID >"
+    				}],
+    				"FAILED": [{
+    					"DISPLAY": "< Insert TextUID >"
+    				}],
+    				"PERCENT": 5
+    			}
+    		}
+    	},
 
-"Options": {
-  "<OPTION ID>": ["Options", "To", "Choose", "From"],
-
-
-  "DisplayItem1": "This is printed when calling DisplayItem1"
-}
-}
+    	"Options": {
+    		"<OPTION ID>": ["Options", "To", "Choose", "From"]
+    	},
+    	"Text": {
+    		"<TextUID>": "INSERT_TEXT_HERE"
+    	}
+    }
--- encounters.py
+++ encounters.py
@@ -35,30 +35,30 @@ class encounters:
 				else:
 					print(alphabet[count]+") " + x)
 				count += 1
-	def printRandomStory(self):
-		storyID = (random.randint(1,len(self.StoryData["RandomStories"])))-1
-		print(self.StoryData["RandomStories"][storyID]["TEXT"])
+	def printRandomStory(self,group = "Encounters"):
+		storyID = (random.randint(1,len(self.StoryData["RandomStories"][group])))-1
+		print(self.StoryData["RandomStories"][group][storyID]["TEXT"])
 		return StoryID
 
-	def doAction(self,storyID,actionNumber,Random = True):
+	def doAction(self,storyID,actionNumber,group,Random = True):
 		optionState = "SUCCESS"
-		if Random = True:
-			execData = self.StoryData["RandomStories"][storyID]["OPTIONS"]
+		if Random == True:
+			execData = self.StoryData["RandomStories"][group][storyID]["OPTIONS"]
 		else:
 			execData = self.StoryData["Stories"][storyID]["OPTIONS"]
 		if "PERCENT" in execData:
 			percent = execData["PERCENT"]
 			if random.randint(1,100) <= percent:
 				pass
-				]
+				
 			else:
 				optionState = "FAILED"
 		try:
 			for x in execData[optionState]:
-				for i in ["SHOW","BUFF"]
+				for i in ["SHOW","BUFF"]:
 					try:
 						Command = x[i]
-						exec("self." + i.lower() + "("Command")")
+						exec("self." + i.lower() + "(" + Command + ")")
 					except:
 						pass
 		except:
@@ -66,142 +66,142 @@ class encounters:
 
 
 
-
-
-
-	def printStorys(self,number):
-		story = ["""
-		While treading through a thick jungle, you meet behind to face with a
-		camouflage rhino. It seems to be slightly disturbed, but not aware of you.
-		What will you do?
-		""",
-		"""
-		The Beast looks at you wearily, wondering what you plan on doing with
-		drooping ears. It looks as if it is a calf separated from it's crash.
-		""",
-		"""
-		As you charge toward it, you feel guilty of poaching.
-		You realize that it has evolved to hide from poachers
-		that kill rhinos for their horns. Finally, still with
-		a thoughtful doubt in your mind, you stab the beast and
-		it falls to the ground with a sad final shriek. Still
-		feeling inhumane, you take the rhino's horn.
-		""",
-
-		]
-
-
-
-
-		insert = """
-
-			 		  	   V V V
-					   		V V
-
-				"""
-
-
-
-
-
-
-
-
-	def enc1(self,errorCode = ""):
-
-		if enc1 == 1
-			creature_enc()
-		print("""
-
-		While treading through a thick jungle, you meet behind to face with a
-		camouflage rhino. It seems to be slightly disturbed, but not aware of you.
-		What will you do?
-
-	 					  V V V
-						   V V
-
-
-				a) RUN!!! |works - 50%|
-				b) Hide |works - {}%|
-				c) Encounter
-
-			{}
-
-		""".format(int(self.clever * 2), errorCode))
-
-		response = askPrompt()
-			if response = "c"
-				print("""
-				The Beast looks at you wearily, wondering what you plan on doing with
-				drooping ears. It looks as if it is a calf separated from it's crash.
-
-								  V V V
-								   V V
-
-				a) Throw him a small piece of your food and leave slowly (+ Wisdom)
-				b) Pet
-				c) Attack	\*
-				""".format(int(self.strength * 2), errorCode))
-
-				response = askPrompt()
-				if response = 'c'
-					do_atactk()
-
-				if atk == 1
-					print("""
-					As you charge toward it, you feel guilty of poaching.
-					You realize that it has evolved to hide from poachers
-					that kill rhinos for their horns. Finally, still with
-					a thoughtful doubt in your mind, you stab the beast and
-					it falls to the ground with a sad final shriek. Still
-					feeling inhumane, you take the rhino's horn.
-					""")
-				roll()
-
-				if atk == 0
-					print("""
-
-					As you prepare to attack the rhino.
-					In notices you, and with one swift
-					move, it charges you and knocks you
-					into a tree. You pass out immediately.
-
-						 	  `::-----------:.
-						   .::-			     .::.
-						`::.					.:-
-					   ::					   	  `/.
-					  :-							-/
-					  +./						   -:-
-					 :- y						  -+ +
-					 o `s						  .o o
-					 o o.						   s o
-					 o/: `..--`					 o.o
-					 ++.odNNNNmddo` `---.  .-/y++/:``.+
-					 .+:MMMMMMMMMMo ....:`+mMMMMMMMm//.
-					.:-:MMMMMMMMMMN. `	  NMMMMMMMMMo/.
-					o` `dMMMMMMMMmo`+dd/ `NMMMMMMMMm`+`
-					o   .+yhddhs:. /NMMy  -yNMMMMNy. .+
-					:-			   mMMMM:   .:+o+:   `+
-					 /.```		  oMMMMMd			  /`
-					  ..-+-/`	  +NNmMNy	  `````::
-					  .::+ so	  ..`:-	      o-++++`
-					  /.   ss`			    -N `..+
-					   -/` +Mms:---:-.-:-.-:sdh  /-
-						 +`.MMm+oy.o.s`s++y+MM: /.
-						  +`+MMMhyosoyoss+NMMo :-
-						  `+ /dy+---...--:oy-  o
-						   :-				`+`
-							-:--:--------:--::
-					""")
-					time.sleep(1000000)
-
-			if response = 'b'
-				Print_1
-
-
-
-
-
-	#creature encounter
-
-Screen = encounters()
+#
+#
+#
+# 	def printStorys(self,number):
+# 		story = ["""
+# 		While treading through a thick jungle, you meet behind to face with a
+# 		camouflage rhino. It seems to be slightly disturbed, but not aware of you.
+# 		What will you do?
+# 		""",
+# 		"""
+# 		The Beast looks at you wearily, wondering what you plan on doing with
+# 		drooping ears. It looks as if it is a calf separated from it's crash.
+# 		""",
+# 		"""
+# 		As you charge toward it, you feel guilty of poaching.
+# 		You realize that it has evolved to hide from poachers
+# 		that kill rhinos for their horns. Finally, still with
+# 		a thoughtful doubt in your mind, you stab the beast and
+# 		it falls to the ground with a sad final shriek. Still
+# 		feeling inhumane, you take the rhino's horn.
+# 		""",
+#
+# 		]
+#
+#
+#
+#
+# 		insert = """
+#
+# 			 		  	   V V V
+# 					   		V V
+#
+# 				"""
+#
+#
+#
+#
+#
+#
+#
+#
+# 	def enc1(self,errorCode = ""):
+#
+# 		if enc1 == 1
+# 			creature_enc()
+# 		print("""
+#
+# 		While treading through a thick jungle, you meet behind to face with a
+# 		camouflage rhino. It seems to be slightly disturbed, but not aware of you.
+# 		What will you do?
+#
+# 	 					  V V V
+# 						   V V
+#
+#
+# 				a) RUN!!! |works - 50%|
+# 				b) Hide |works - {}%|
+# 				c) Encounter
+#
+# 			{}
+#
+# 		""".format(int(self.clever * 2), errorCode))
+#
+# 		response = askPrompt()
+# 			if response = "c"
+# 				print("""
+# 				The Beast looks at you wearily, wondering what you plan on doing with
+# 				drooping ears. It looks as if it is a calf separated from it's crash.
+#
+# 								  V V V
+# 								   V V
+#
+# 				a) Throw him a small piece of your food and leave slowly (+ Wisdom)
+# 				b) Pet
+# 				c) Attack	\*
+# 				""".format(int(self.strength * 2), errorCode))
+#
+# 				response = askPrompt()
+# 				if response = 'c'
+# 					do_atactk()
+#
+# 				if atk == 1
+# 					print("""
+# 					As you charge toward it, you feel guilty of poaching.
+# 					You realize that it has evolved to hide from poachers
+# 					that kill rhinos for their horns. Finally, still with
+# 					a thoughtful doubt in your mind, you stab the beast and
+# 					it falls to the ground with a sad final shriek. Still
+# 					feeling inhumane, you take the rhino's horn.
+# 					""")
+# 				roll()
+#
+# 				if atk == 0
+# 					print("""
+#
+# 					As you prepare to attack the rhino.
+# 					In notices you, and with one swift
+# 					move, it charges you and knocks you
+# 					into a tree. You pass out immediately.
+#
+# 						 	  `::-----------:.
+# 						   .::-			     .::.
+# 						`::.					.:-
+# 					   ::					   	  `/.
+# 					  :-							-/
+# 					  +./						   -:-
+# 					 :- y						  -+ +
+# 					 o `s						  .o o
+# 					 o o.						   s o
+# 					 o/: `..--`					 o.o
+# 					 ++.odNNNNmddo` `---.  .-/y++/:``.+
+# 					 .+:MMMMMMMMMMo ....:`+mMMMMMMMm//.
+# 					.:-:MMMMMMMMMMN. `	  NMMMMMMMMMo/.
+# 					o` `dMMMMMMMMmo`+dd/ `NMMMMMMMMm`+`
+# 					o   .+yhddhs:. /NMMy  -yNMMMMNy. .+
+# 					:-			   mMMMM:   .:+o+:   `+
+# 					 /.```		  oMMMMMd			  /`
+# 					  ..-+-/`	  +NNmMNy	  `````::
+# 					  .::+ so	  ..`:-	      o-++++`
+# 					  /.   ss`			    -N `..+
+# 					   -/` +Mms:---:-.-:-.-:sdh  /-
+# 						 +`.MMm+oy.o.s`s++y+MM: /.
+# 						  +`+MMMhyosoyoss+NMMo :-
+# 						  `+ /dy+---...--:oy-  o
+# 						   :-				`+`
+# 							-:--:--------:--::
+# 					""")
+# 					time.sleep(1000000)
+#
+# 			if response = 'b'
+# 				Print_1
+#
+#
+#
+#
+#
+# 	#creature encounter
+#
+# Screen = encounters()
